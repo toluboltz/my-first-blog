d21e3fa62b0db63f72f631858226a1f3915dd24f
Tolulope Akinbulire <toluboltz@gmail.com>
2024-03-03 15:06:07 -0500

    Add view and template for detailed blog post as well as CSS for the site. Also truncate the number of words for each post in the list on the homepage to 50 words

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name= "blog"
+
 urlpatterns = [
-    path('', views.post_list, name="post_list") 
+    path('', views.post_list, name="post_list"),
+    path("post/<int:post_id>/", views.post_detail, name="post_detail"), 
 ]
