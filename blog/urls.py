de45a1aa7be305f62ab9b2f4a1e667111a785e70
Tolulope Akinbulire <toluboltz@gmail.com>
2024-03-08 13:58:03 -0500

    Add views to create/edit blog post on webpage

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,8 @@ from . import views
 app_name= "blog"
 
 urlpatterns = [
-    path('', views.post_list, name="post_list"),
-    path("post/<int:post_id>/", views.post_detail, name="post_detail"), 
+    path("", views.post_list, name="post_list"),
+    path("post/<int:post_id>/", views.post_detail, name="post_detail"),
+    path("post/new", views.post_new, name="post_new"),
+    path("post/<int:post_id>/edit", views.post_edit, name="post_edit"),
 ]
